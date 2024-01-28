9b3d35a624c148f6abb07d93e6ebfd6386d5c5d1
Jiahui <forestlin333@gmail.com>
2024-01-28 17:53:25 +0100

    Get the data from flickr

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,28 @@
 import express from "express";
+import axios from 'axios';
 
 const app = express();
 const port = 8000;
 
-app.get("/", (req, res) => {
-    res.send("Hello from express lalala");
+app.get('/api/images', async (req, res) => {
+    try {
+        const tags = req.query.tags;
+        const response = await axios.get('https://api.flickr.com/services/feeds/photos_public.gne', {
+            params: { format: 'json', nojsoncallback: 1, tags },
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        console.log(response.data);
+        res.json(response.data.items);
+    } catch (error) {
+        console.error('Error fetching data from Flickr:', error);
+        res.status(500).send('Error fetching data from Flickr');
+    }
 });
 
+// Start the server
 app.listen(port, () => {
-return console.log(`Express server is listening on http://localhost:${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
+
+export default app;
